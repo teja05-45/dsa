ebbb3947de7486a249ae0a776d69e255909eddcd
teja05-45 <tejamatta05@gmail.com>
2026-09-10 11:28:02 +0530

    valid palindrome

---
+++ neetcode/strings/two pointers/valid palindrome.py
@@ -0,0 +1,21 @@
+class Solution():
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        left=0
+        right=len(s)-1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left+=1
+            while left < right and not s[right].isalnum():
+                right-=1
+            if s[left].lower()!=s[right].lower():
+                return False
+            left+=1
+            right-=1
+
+        return True
+obj= Solution()
+print(obj.isPalindrome("A man, a plan, a canal: Panama"))  # Output: True
